8bec9ed4b0b37953009bfcad2c179e50ee9b8340
KingFillip <fillipkomornik@icloud.com>
2014-10-21 10:16:30 -0500

    Working user interface

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = '9923cb63154971ac14ce397d8473401eb07a12bc15b9648ab9c50a4a5de9b39106d7864bb2cbfaf58225b4967f60c569477187c3fe564b3f9e6d80a3e755787e'
---
+++ db/migrate/20141018225654_add_school_to_users.rb
@@ -0,0 +1,8 @@
+class AddSchoolToUsers < ActiveRecord::Migration
+  def change
+  	change_table :users do |t|
+      t.string :school
+
+    end
+  end
+end
